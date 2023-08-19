ae3d3dd4820d4b6ddf8ea3e1e709ed29f4f02b49
Lukman <lukmanagungprakoso@gmail.com>
2023-08-19 21:25:13 +0700

    push again

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -5,6 +5,7 @@ import (
 	"go-web-native/model/categorymodel"
 	"html/template"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -49,6 +50,45 @@ func Add(w http.ResponseWriter, r *http.Request) {
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		tmpl, err := template.ParseFiles("views/category/edit.html")
+		if err != nil {
+			panic(err)
+		}
+
+		idString := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category := categorymodel.Detail(id)
+		data := map[string]any{
+			"category": category,
+		}
+
+		tmpl.Execute(w, data)
+	}
+
+	if r.Method == "POST" {
+		var category entities.Category
+
+		idString := r.FormValue("id")
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			panic(err)
+		}
+
+		category.Name = r.FormValue("name")
+		category.UpdatedAt = time.Now()
+
+		if ok := categorymodel.Update(id, category); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, "/categories", http.StatusSeeOther)
+	}
 
 }
 func Delete(w http.ResponseWriter, r *http.Request) {
